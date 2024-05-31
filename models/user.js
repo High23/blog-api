9b39e187138043185ce7027387f2e77e24af4439
High23 <warenasb2@gmail.com>
2024-05-31 00:59:35 -0700

    Adjusted schema

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: {type: String, minLength: 3, maxLength: 100, required: true},
+    username: {type: String, minLength: 1, maxLength: 100, required: true},
     password: {type: String, minLength: 7, required: true},
-    role: {type: String, enum: ['author', 'user'], required: 'user'},
+    role: {type: Boolean},
 });
 
 module.exports = mongoose.model("User", UserSchema);
