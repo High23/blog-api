298bae98edd538e69bf95686935a5ba4ab1792f4
High23 <warenasb2@gmail.com>
2024-06-11 23:25:15 -0700

    Changed 'type' field name to 'published'

--- models/post.js
+++ models/post.js
@@ -5,7 +5,7 @@ const postSchema = new Schema({
     title: {type: String, minLength: 1, maxLength: 75, required: true},
     text: {type: String, minLength: 2, required: true},
     author: {type: Schema.Types.ObjectId, ref: "User", default: undefined},
-    type: {type: String, enum: ['published', 'not published'], required: 'published'}
+    published: {type: Boolean, default: true}
 });
 
 module.exports = mongoose.model("Post", postSchema)
