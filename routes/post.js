447e70c08973c83056e0980a9288e13006a01745
High23 <warenasb2@gmail.com>
2024-06-12 23:49:13 -0700

    Added routes for comment creation and single post

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const router = express.Router();
 const postController = require("../controllers/postController");
+const commentController = require('../controllers/commentController');
 
 router.get("/create", postController.createGet)
 
 router.post("/create", postController.createPost);
+
+router.post("/:postId/comment/create", commentController.createCommentPost)
+
+router.get("/:postId", postController.getPost)
 
 module.exports = router;
