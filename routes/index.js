b41ebc674b5c46d89a7da6a0c739b0d6cf2c9288
High23 <warenasb2@gmail.com>
2024-06-13 00:09:58 -0700

    Adjust index get route to get all posts

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const { body, validationResult } = require('express-validator');
-const expressAsyncHandler = require('express-async-handler');
 const router = express.Router();
+const postController = require('../controllers/postController')
 
-router.get('/', expressAsyncHandler( async function(req, res, next) {
-    res.json('Route not done')
-}));
+router.get('/', postController.getAllPosts);
 
 module.exports = router;
