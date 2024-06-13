77bfcfae6bc4bf68dd24770fe414e4a2a6a95265
High23 <warenasb2@gmail.com>
2024-06-12 23:47:54 -0700

    Created post comment creation function handler

---
+++ controllers/commentController.js
@@ -0,0 +1,37 @@
+const {body, validationResult} = require("express-validator");
+const asyncHandler = require("express-async-handler");
+const jwt = require('jsonwebtoken');
+const { verifyTokenHeaderExists } = require("../public/javascripts/token");
+const Comment = require('../models/comment');
+
+require('dotenv').config()
+
+exports.createCommentPost = [
+    verifyTokenHeaderExists,
+
+    body('text')
+    .trim()
+    .isLength({min: 2, max: 250})
+    .escape(),
+
+    asyncHandler(async function(req, res, next) {
+        const errors = validationResult(req);
+        const userId = jwt.verify(req.token, process.env.SECRET).user._id;
+        const comment = new Comment({
+            text: req.body.text,
+            date: new Date(),
+            author: userId,
+            post: req.params.postId,
+        });
+
+        if (!errors.isEmpty()) {
+            res.json({
+              errors: errors.array()
+            });
+            return
+        } else {
+            await comment.save()
+            res.sendStatus(200);
+        }
+    })
+]
