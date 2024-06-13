e54e8a336711d43c18738d30a76700bf3e044981
High23 <warenasb2@gmail.com>
2024-06-12 22:55:09 -0700

    Adjusted field name

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     username: {type: String, minLength: 1, maxLength: 100, required: true},
     password: {type: String, minLength: 7, required: true},
-    role: {type: Boolean},
+    author: {type: Boolean},
 });
 
 module.exports = mongoose.model("User", UserSchema);
