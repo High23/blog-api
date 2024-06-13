bb8f40164969d43a6ba1e4397124d4d85809f1b3
High23 <warenasb2@gmail.com>
2024-06-12 22:54:31 -0700

    Added fields to schemas

--- models/comment.js
+++ models/comment.js
@@ -3,8 +3,9 @@ const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
     text: {type: String, minLength: 2, maxLength: 250, required: true},
-    date: {type: String, required: true},
+    date: {type: Date},
     author: {type: Schema.Types.ObjectId, ref: "User", default: undefined},
+    post: {type: Schema.Types.ObjectId, ref: "Post", default: undefined},
 });
 
 module.exports = mongoose.model("Comment", commentSchema);
--- models/post.js
+++ models/post.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
     title: {type: String, minLength: 1, maxLength: 75, required: true},
     text: {type: String, minLength: 2, required: true},
+    date: {type: Date},
     author: {type: Schema.Types.ObjectId, ref: "User", default: undefined},
     published: {type: Boolean, default: true}
 });
