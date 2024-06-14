ce5cb741b22e3015ea87d80ee48d45ce68e8785c
High23 <warenasb2@gmail.com>
2024-06-13 23:02:56 -0700

    Added last updated field to posts models

--- models/post.js
+++ models/post.js
@@ -5,6 +5,7 @@ const postSchema = new Schema({
     title: {type: String, minLength: 1, maxLength: 75, required: true},
     text: {type: String, minLength: 2, required: true},
     date: {type: Date},
+    lastUpdated: {type: Date},
     author: {type: Schema.Types.ObjectId, ref: "User", default: undefined},
     published: {type: Boolean, default: true}
 });
