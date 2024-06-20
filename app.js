53f50cd2e06418cd90fb23b671f589af9fdcccd4
High23 <warenasb2@gmail.com>
2024-06-19 21:00:38 -0700

    Added user route

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const indexRouter = require('./routes/index');
 const signUpRouter = require('./routes/sign-up');
 const loginRouter = require('./routes/login');
 const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
 
 require('dotenv').config();
 
@@ -45,6 +46,7 @@ app.use('/', indexRouter);
 app.use('/sign-up', signUpRouter);
 app.use('/login', loginRouter);
 app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
