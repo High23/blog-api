b8960da9ad9f8bb55e92042b034aa19ed95e776c
High23 <warenasb2@gmail.com>
2024-07-15 23:29:34 -0700

    Removed a useless route

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -3,8 +3,6 @@ const express = require('express');
 const router = express.Router();
 const signUpController = require('../controllers/sign-upController');
 
-router.get('/', signUpController.createUserGet);
-
 router.post('/', signUpController.createUserPost);
 
 module.exports = router;
